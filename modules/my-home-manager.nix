70a9ff2912f6ab43da551945f2087cc2c93b6a92
Elis Hirwing <elis@hirwing.se>
2021-08-11 10:10:08 +0200

    modules/home-manager: Configure weechat

--- modules/my-home-manager.nix
+++ modules/my-home-manager.nix
@@ -100,6 +100,177 @@ in
           }];
         };
 
+        # Enable weechat configuration
+        programs.weechat.enable = (config.networking.hostName == "vps04");
+        programs.weechat.scripts = with pkgs.weechatScripts; [
+          colorize_nicks
+          weechat-matrix
+
+          (pkgs.stdenv.mkDerivation {
+            pname = "zerotab";
+            version = "1.5";
+            src = pkgs.fetchurl {
+              url = "https://github.com/weechat/scripts/raw/dff455a80e065ad2131cf6cab190821b4ec415a3/python/zerotab.py";
+              sha256 = "1qp3h8kd35m8ywskizpgcs53kdj7y1qm0y9kcy9wjvhny0g35p4y";
+            };
+            dontUnpack = true;
+            passthru.scripts = [ "zerotab.py" ];
+            installPhase = ''
+              install -D $src $out/share/zerotab.py
+            '';
+          })
+
+          (pkgs.stdenv.mkDerivation {
+            pname = "title";
+            version = "0.9";
+            src = pkgs.fetchurl {
+              url = "https://github.com/weechat/scripts/raw/1ec7ba80f5fec074c86b5e2054537e5f94fde1a7/python/title.py";
+              sha256 = "1h8mxpv47q3inhynlfjm3pdjxlr2fl06z4cdhr06kpm8f7xvz56p";
+            };
+            dontUnpack = true;
+            passthru.scripts = [ "title.py" ];
+            installPhase = ''
+              install -D $src $out/share/title.py
+            '';
+          })
+
+          (pkgs.stdenv.mkDerivation {
+            pname = "screen_away";
+            version = "0.16";
+            src = pkgs.fetchurl {
+              url = "https://github.com/weechat/scripts/raw/08f39d6892b0f2d0d14e270424be80ffdc870e9d/python/screen_away.py";
+              sha256 = "1m48n23vv48adb2p8zcvvnk5qwl8h249v0nmdrn8zajkhgprjywp";
+            };
+            dontUnpack = true;
+            passthru.scripts = [ "screen_away.py" ];
+            installPhase = ''
+              install -D $src $out/share/screen_away.py
+            '';
+          })
+
+          (pkgs.stdenv.mkDerivation {
+            pname = "chanop";
+            version = "0.3.4";
+            src = pkgs.fetchurl {
+              url = "https://github.com/weechat/scripts/raw/08f39d6892b0f2d0d14e270424be80ffdc870e9d/python/chanop.py";
+              sha256 = "0d84dcsbywxh5cdbq9bbgr7afjm9mm99lhz8fhl8fcnhdji8mr6p";
+            };
+            dontUnpack = true;
+            passthru.scripts = [ "chanop.py" ];
+            installPhase = ''
+              install -D $src $out/share/chanop.py
+            '';
+          })
+
+          (pkgs.stdenv.mkDerivation {
+            pname = "nickregain";
+            version = "1.1.1";
+            src = pkgs.fetchurl {
+              url = "https://github.com/weechat/scripts/raw/d1b288d3c49b5e6deb14f61f0acaf25aecc24fdb/perl/nickregain.pl";
+              sha256 = "0m5hdlzyl1xh61zh87lhjjpz6kdgjm3h651s5mlmhs8n2ff3mnjw";
+            };
+            dontUnpack = true;
+            passthru.scripts = [ "nickregain.pl" ];
+            installPhase = ''
+              install -D $src $out/share/nickregain.pl
+            '';
+          })
+
+          (pkgs.stdenv.mkDerivation {
+            pname = "recoverop";
+            version = "0.1.2";
+            src = pkgs.fetchurl {
+              url = "https://github.com/weechat/scripts/raw/396c12c65c8667dac15354d4b4b2f0f2a4183fdc/perl/recoverop.pl";
+              sha256 = "17ccxfzcms37ypb8w2fb7kpjxgrxmdkq8jml0q0qby1a2gky7iyz";
+            };
+            dontUnpack = true;
+            passthru.scripts = [ "recoverop.pl" ];
+            installPhase = ''
+              install -D $src $out/share/recoverop.pl
+            '';
+          })
+        ];
+        programs.weechat.configs = {
+          alias.cmd = {
+            # Unset default aliases
+            AAWAY = null;  ANICK = null;   BEEP = null; BYE = null;   C = null;
+            CL = null;     CLOSE = null;   CHAT = null; EXIT = null;  IG = null;
+            J = null;      K = null;       KB = null;   LEAVE = null; M = null;
+            MSGBUF = null; MUB = null;     N = null;    Q = null;     REDRAW = null;
+            SAY = null;    SIGNOFF = null; T = null;    UB = null;    UMODE = null;
+            V = null;      W = null;       WC = null;   WI = null;    WII = null;
+            WM = null;     WW = null;
+
+            # Set my own aliases
+            anick = "allserv /nick";
+            bail = "/me bailar";
+            clear = "buffer clear";
+            close = "buffer close";
+            exit = "quit";
+            j = "join";
+            n = "names";
+            q = "query";
+            t = "topic";
+            # This one doesn't work, it cuts of after the first ;
+            # weeclear = "anick etu; buffer weechat; allserv /buffer clear; buffer clear; version; uptime; buffer close relay.relay.list";
+          };
+          irc.server_default.nicks = [ "etu" "etu_" "_etu" "_etu_" ];
+          irc.server_default.username = "etu";
+          irc.server = {
+            beanjuice = {
+              addresses = "irc.beanjuice.me/6697";
+              autoconnect = true;
+              autojoin = "#vikings";
+              ssl = true;
+              ssl_verify = false;
+            };
+            efnet = {
+              addresses = [ "irc.swepipe.se/6697" "efnet.port80.se/6697" "efnet.xs4all.nl/6697" ];
+              autoconnect = true;
+              autojoin = "#mellovision";
+              ssl = true;
+              ssl_verify = false;
+            };
+            geekshed = {
+              addresses = "irc.geekshed.net/6697";
+              ssl = true;
+              autoconnect = true;
+              autojoin = "#jupiterbroadcasting";
+            };
+            hackint = {
+              addresses = "irc.eu.hackint.org/6697";
+              autoconnect = true;
+              autojoin = "#tvl";
+              sasl_mechanism = "plain";
+              sasl_password = ''''${sec.hackint.password}'';
+              sasl_username = "etu";
+              ssl = true;
+            };
+            oftc = {
+              addresses = "irc.oftc.net/6697";
+              autoconnect = true;
+              autojoin = [ "#flummon" "#ix" ];
+              ssl = true;
+              ssl_cert = "%h/ssl/etu.pem";
+              ssl_verify = true;
+            };
+          };
+          logger.file.auto_log = false;
+          relay.network.password = ''''${sec.relay.password}'';
+          relay.port.weechat = 8001;
+          colorize_nicks.look.colorize_input = true;
+          weechat = {
+            look.item_time_format = "%H:%M:%S";
+            look.prefix_suffix = "|";
+            look.read_marker_string = "=";
+            look.read_marker_always_show = true;
+            look.nick_completer = ": ";
+            look.chat_read_marker = "darkgray";
+            look.chat_inactive_buffer = "darkgray";
+            look.chat_time_delimiters = "default"; # Default is "brown"
+          };
+        };
+
         home.stateVersion = "20.09";
       };
   };
